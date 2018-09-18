2321870c6b7a1da27032cd465e5407f9ad306549
Debangshu Dasgupta <Debangshu.Dasgupta@gmail.com>
2018-09-17 22:32:39 -0400

    Initial

---
+++ src/main/java/com/acacia/akka/sample1/SampleApp1.java
@@ -0,0 +1,46 @@
+package com.acacia.akka.sample1;
+
+import akka.actor.AbstractLoggingActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SampleApp1 {
+
+    static class Counter extends AbstractLoggingActor {
+        private int counter = 0;
+
+        // protocol
+        static class Message {
+
+        }
+
+        public Receive createReceive() {
+            return receiveBuilder().match(Counter.Message.class, message -> onMesssage(message)).build();
+        }
+
+        private void onMesssage(Counter.Message message) throws InterruptedException {
+//            Thread.sleep(1000);
+            counter++;
+            log.info("Increased counter {}", counter);
+        }
+
+        public static Props props() {
+            return Props.create(Counter.class);
+        }
+    }
+
+    public static void main(String[] args) {
+        log.info("main");
+        ActorSystem actorSystem = ActorSystem.create("sample1");
+
+        for (int i = 0; i < 150; i++) {
+            ActorRef counter = actorSystem.actorOf(Counter.props(), String.valueOf(i));
+            counter.tell(new Counter.Message(), ActorRef.noSender());
+        }
+
+        log.info("Done");
+    }
+}
---
+++ src/main/java/com/acacia/akka/sample2/SampleApp2.java
@@ -0,0 +1,46 @@
+package com.acacia.akka.sample2;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SampleApp2 {
+
+    static class Alarm extends AbstractActor {
+
+        static class Activity {
+
+        }
+
+        static class Disable {
+
+        }
+
+        static class Enable {
+
+        }
+
+        @Override
+        public Receive createReceive() {
+            return receiveBuilder()
+                    .match(Disable.class, disable -> log.info("Disabled"))
+                    .match(Enable.class, enable -> log.info("Enabled"))
+                    .build();
+        }
+
+        public static Props props() {
+            return Props.create(Alarm.class);
+        }
+    }
+
+    public static void main(String[] args) {
+        log.info("main");
+        ActorSystem sample2 = ActorSystem.create("sample2");
+        ActorRef alarm = sample2.actorOf(Alarm.props(), "alarm");
+        alarm.tell(new Alarm.Enable(), ActorRef.noSender());
+        alarm.tell(new Alarm.Disable(), ActorRef.noSender());
+    }
+}
